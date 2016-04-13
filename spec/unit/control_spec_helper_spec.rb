57353a4989328b47d375d92b7627f511803a98dd
Eric Shamow <alice@example.com>
2016-04-13 13:06:55 -0700

    More passing tests

--- spec/unit/control_spec_helper_spec.rb
+++ spec/unit/control_spec_helper_spec.rb
@@ -18,6 +18,33 @@ describe 'control_spec_helper' do
     expect(@dummy_class.basepath).to eq('dist')
   end
 
+  it 'should have a default basebranch' do
+    expect(@dummy_class.basebranch).to eq('master')
+  end
+
+  it 'should allow you to set basebranch' do
+    @dummy_class.basebranch = 'production'
+    expect(@dummy_class.basebranch).to eq('production')
+  end
+
+  describe 'when debug environmental variable is set' do
+    it 'should print properly-formatted debugging messages' do
+      cached_env_debug = ENV['debug']
+      ENV['debug'] = 'true'
+      expect { @dummy_class.debug('spec') }.to output("DEBUG: spec\n").to_stdout
+      ENV['debug'] = cached_env_debug
+    end
+  end
+
+  describe 'when debug environmental variable is not set' do
+    it 'should suppress debugging messages' do
+      cached_env_debug = ENV['debug']
+      ENV['debug'] = ''
+      expect { @dummy_class.debug('spec') }.to_not output('DEBUG: spec').to_stdout
+      ENV['debug'] = cached_env_debug
+    end
+  end
+
   it 'should print the puppet command' do
     cmd = "puppet apply manifests/site.pp \\\n      --modulepath $(echo `pwd`/modules:`pwd`/site) --hiera_config hiera.yaml"
     expect(@dummy_class.puppet_cmd).to eq(cmd)
