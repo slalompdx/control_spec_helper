393d816e13f17e577570acb718774fdb5d97ced1
Eric Shamow <alice@example.com>
2016-04-15 16:23:54 -0700

    3 tests remaining

--- spec/unit/control_spec_helper_spec.rb
+++ spec/unit/control_spec_helper_spec.rb
@@ -401,15 +401,91 @@ describe 'control_spec_helper' do
   end
 
   describe 'when spec_clean is called' do
-    describe 'when debug environmental variable is set' do
-      it 'should print its current project directory'
-      it 'should print its actual working directory'
+    before(:each) do
+      allow(@dummy_class).to receive(:project_root).and_return('/')
+      allow(@dummy_class).to receive(:profile_path).and_return('/tmp')
+      allow(Dir).to receive(:pwd).and_return('/foo')
     end
-    it 'should abort if fixtures is empty'
-    it 'should abort is fixtures is null'
-    it 'should abort if modules is empty'
-    it 'should abort if modules is null'
-    it 'calls the appropriate command to remove fixtures'
-    it 'calls the appropriate command to remove modules'
+
+    describe 'when debug environmental variable is set' do
+
+      it 'should print its current project directory' do
+        allow(@dummy_class).to receive(:`).with('rm -rf /tmp/spec/fixtures/modules/*')
+        allow(@dummy_class).to receive(:`).with('rm -rf /modules/*')
+        cached_env_debug = ENV['debug']
+        ENV['debug'] = 'true'
+        expect { @dummy_class.spec_clean }.to output(/DEBUG: cd to \//).to_stderr
+        ENV['debug'] = cached_env_debug
+      end
+
+      it 'should print its actual working directory' do
+        allow(@dummy_class).to receive(:`).with('rm -rf /tmp/spec/fixtures/modules/*')
+        allow(@dummy_class).to receive(:`).with('rm -rf /modules/*')
+        cached_env_debug = ENV['debug']
+        ENV['debug'] = 'true'
+        expect { @dummy_class.spec_clean }.to output(/DEBUG: cd to \/foo/).to_stderr
+        ENV['debug'] = cached_env_debug
+      end
+    end
+    describe 'when debug environmental variable is not set' do
+
+      it 'should not print its current project directory' do
+        allow(@dummy_class).to receive(:`).with('rm -rf /tmp/spec/fixtures/modules/*')
+        allow(@dummy_class).to receive(:`).with('rm -rf /modules/*')
+        cached_env_debug = ENV['debug']
+        ENV.delete('debug')
+        expect { @dummy_class.spec_clean }.to_not output(/DEBUG: cd to \//).to_stderr
+        ENV['debug'] = cached_env_debug
+      end
+
+      it 'should not print its actual working directory' do
+        allow(@dummy_class).to receive(:`).with('rm -rf /tmp/spec/fixtures/modules/*')
+        allow(@dummy_class).to receive(:`).with('rm -rf /modules/*')
+        cached_env_debug = ENV['debug']
+        ENV.delete('debug')
+        expect { @dummy_class.spec_clean }.to_not output(/DEBUG: cd to \//).to_stderr
+        ENV['debug'] = cached_env_debug
+      end
+    end
+
+    it 'should abort if fixtures is empty' do
+      allow(@dummy_class).to receive(:`).with('rm -rf /tmp/spec/fixtures/modules/*')
+      allow(@dummy_class).to receive(:`).with('rm -rf /modules/*')
+      fixtures = ''
+      expect { @dummy_class.spec_clean }.to abort
+    end
+
+    it 'should abort if fixtures is null' do
+      allow(@dummy_class).to receive(:`).with('rm -rf /tmp/spec/fixtures/modules/*')
+      allow(@dummy_class).to receive(:`).with('rm -rf /modules/*')
+      fixtures = nil
+      expect { @dummy_class.spec_clean }.to abort
+    end
+
+    it 'should abort if modules is empty' do
+      allow(@dummy_class).to receive(:`).with('rm -rf /tmp/spec/fixtures/modules/*')
+      allow(@dummy_class).to receive(:`).with('rm -rf /modules/*')
+      modules = ''
+      expect { @dummy_class.spec_clean }.to abort
+    end
+
+    it 'should abort if modules is null' do
+      allow(@dummy_class).to receive(:`).with('rm -rf /tmp/spec/fixtures/modules/*')
+      allow(@dummy_class).to receive(:`).with('rm -rf /modules/*')
+      modules = nil
+      expect { @dummy_class.spec_clean }.to abort
+    end
+
+    it 'calls the appropriate command to remove fixtures' do
+      allow(@dummy_class).to receive(:`).with('rm -rf /modules/*')
+      expect(@dummy_class).to receive(:`).with('rm -rf /tmp/spec/fixtures/modules/*')
+      @dummy_class.clean_spec
+    end
+
+    it 'calls the appropriate command to remove modules' do
+      expect(@dummy_class).to receive(:`).with('rm -rf /modules/*')
+      allow(@dummy_class).to receive(:`).with('rm -rf /tmp/spec/fixtures/modules/*')
+      @dummy_class.clean_spec
+    end
   end
 end
