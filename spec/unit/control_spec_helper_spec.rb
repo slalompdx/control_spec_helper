16c0df0c2ebe9cf65652d229d05d43e9b37da386
Eric Shamow <alice@example.com>
2016-04-15 14:32:53 -0700

    Use delete to manage the ENV debug var

--- spec/unit/control_spec_helper_spec.rb
+++ spec/unit/control_spec_helper_spec.rb
@@ -604,10 +604,37 @@ describe 'control_spec_helper' do
 
   describe 'when diff_roles is called' do
     it 'should return a diff from base as a map'
+      allow(@dummy_class).to receive(:diff_from_base).and_return(['/tmp/foo/site/profile/manifests/foo.pp','/tmp/foo/site/role/manifests/bar.pp','/tmp/foo/modules/baz/manifests/baz.pp','/tmp/foo/site/role/manifests/fubar.pp'])
+      expect(@dummy_class).to receive(:diff_roles).and_return(['role::bar','role::fubar'])
+      @dummy_class.diff_roles
+    end
   end
 
   describe 'when diff_profile is called' do
-    it 'should return a diff from base as a map'
+    it 'should return a diff from base as a map' do
+      allow(@dummy_class).to receive(:diff_from_base)
+        .and_return([
+                      'a',
+                      '/tmp/foo/site/profile/manifests/foo.pp',
+                      '/tmp/foo/site/profile/manifests/bar.pp'
+                    ])
+      expect(@dummy_class).to receive(:diff_profile)
+        .and_return(['profile::foo', 'profile::bar'])
+      @dummy_class.diff_profile
+    end
+
+    it 'should ignore classes in base that are not profiles' do
+      allow(@dummy_class).to receive(:diff_from_base)
+        .and_return([
+                      '/tmp/foo/site/profile/manifests/foo.pp',
+                      '/tmp/foo/site/role/manifests/bar.pp',
+                      '/tmp/foo/modules/baz/manifests/baz.pp',
+                      '/tmp/foo/site/profile/manifests/fubar.pp'
+                    ])
+      expect(@dummy_class).to receive(:diff_roles)
+        .and_return(['profile::foo', 'profile::fubar'])
+      @dummy_class.diff_roles
+    end
   end
 
   describe 'when passed a file path' do
@@ -667,13 +694,13 @@ describe 'control_spec_helper' do
       allow(@dummy_class).to receive(:role_path).and_return('/')
       allow(@dummy_class).to receive(:project_root).and_return('/test_path')
       allow(@dummy_class).to receive(:`).with('git grep -l klass')
-        .and_return("manifests/klass.pp\n" +
-                    "manifests/klass/repo.pp\n" +
-                    "manifests/stages.pp\n" +
-                    "spec/classes/klass_spec.rb\n" +
-                    "spec/classes/klass/repo_spec.rb\n" +
+        .and_return("manifests/klass.pp\n" \
+                    "manifests/klass/repo.pp\n" \
+                    "manifests/stages.pp\n" \
+                    "spec/classes/klass_spec.rb\n" \
+                    "spec/classes/klass/repo_spec.rb\n" \
                     'spec/classes/stages_spec.rb')
-      expect{ @dummy_class.roles_that_include('klass') }
+      expect { @dummy_class.roles_that_include('klass') }
         .to output("::klass\n::klass::repo\n::stages\n").to_stdout
     end
 
@@ -690,30 +717,30 @@ describe 'control_spec_helper' do
           allow(@dummy_class).to receive(:project_root).and_return('/test_root')
           allow(@dummy_class).to receive(:basepath)
             .and_return('/test_root/control_spec_helper')
-          expect(@dummy_class.spec_from_class(klass)).to eq('/test_root/profile/spec/klass_spec.rb')
+          expect(@dummy_class.spec_from_class(klass))
+            .to eq('/test_root/profile/spec/klass_spec.rb')
         end
 
         it 'should place a profile spec in the correct path' do
           allow(@dummy_class).to receive(:project_root).and_return('/test_root')
           allow(@dummy_class).to receive(:basepath)
             .and_return('/test_root/control_spec_helper')
-          expect(@dummy_class.spec_from_class(klass)).to eq('/test_root/profile/spec/klass_spec.rb')
+          expect(@dummy_class.spec_from_class(klass))
+            .to eq('/test_root/profile/spec/klass_spec.rb')
         end
 
         it 'should place a role spec in the correct path' do
           allow(@dummy_class).to receive(:project_root).and_return('/test_root')
           allow(@dummy_class).to receive(:basepath)
             .and_return('/test_root/control_spec_helper')
-          expect(@dummy_class.spec_from_class(role_klass)).to eq('/test_root/role/spec/acceptance/klass_spec.rb')
+          expect(@dummy_class.spec_from_class(role_klass))
+            .to eq('/test_root/role/spec/acceptance/klass_spec.rb')
         end
-
       end
     end
   end
 
-  it 'should be able to identify all roles changed since last commit'
   describe 'when r10k is called' do
-
     it 'should call the appropriate r10k command' do
       allow(@dummy_class).to receive(:project_root).and_return('/')
       expect(@dummy_class).to receive(:`).with('r10k puppetfile install')
@@ -733,43 +760,295 @@ describe 'control_spec_helper' do
       end
 
       it 'should print its current project directory' do
-        expect { @dummy_class.r10k }.to output(/cd to \//).to_stderr
+        expect { @dummy_class.r10k }.to output(%r{cd to /}).to_stderr
       end
 
       it 'should print its actual working directory' do
         allow(Dir).to receive(:pwd).and_return('/tmp')
-        expect { @dummy_class.r10k }.to output(/cd to \/tmp/).to_stderr
+        expect { @dummy_class.r10k }.to output(%r{cd to /tmp}).to_stderr
       end
     end
   end
 
   describe 'when profile_fixtures is called' do
     describe 'when debug environmental variable is set' do
-      it 'should print its current profile_path directory'
-      it 'should print its actual working directory'
-    end
-    it 'should create a modules directory inside fixtures'
-    describe 'when a profile directory exists inside fixtures' do
-      it 'should not create a new symlink'
-    end
-    describe 'when a profile directory does not exist inside fixtures' do
-      it 'should create a symlink to the profile directory'
-    end
-    describe 'for each file in the modules directory' do
-      it 'should skip any file that is not a directory'
-      it 'should symlink the module into the fixtures directory'
+      cached_env_debug = ''
+
+      before(:each) do
+        allow(@dummy_class).to receive(:profile_path).and_return('/')
+        allow(File).to receive(:symlink)
+          .with('/', './spec/fixtures/modules/profile')
+        cached_env_debug = ENV['debug']
+        ENV['debug'] = 'true'
+      end
+
+      after(:each) do
+        ENV['debug'] = cached_env_debug
+      end
+
+      context 'if a profile link already exists' do
+        it 'should not try to symlink the profile path' do
+          allow(Dir).to receive(:glob).with('../../modules/*').and_return([])
+          allow(File).to receive(:exists?)
+            .with('./spec/fixtures/modules/profile').and_return(true)
+          expect(File).to_not receive(:symlink)
+            .with('/', './spec/fixtures/modules/profile')
+          allow(FileUtils).to receive(:mkpath).with('./spec/fixtures/modules/')
+          @dummy_class.profile_fixtures
+        end
+      end
+
+      context 'if a profile link does not already exist' do
+        it 'should symlink the profile path' do
+          allow(Dir).to receive(:glob).with('../../modules/*').and_return([])
+          allow(File).to receive(:exists?)
+            .with('./spec/fixtures/modules/profile').and_return(false)
+          expect(File).to receive(:symlink)
+            .with('/', './spec/fixtures/modules/profile')
+          allow(FileUtils).to receive(:mkpath)
+            .with('./spec/fixtures/modules/')
+          @dummy_class.profile_fixtures
+        end
+      end
+
+      context 'when iterating through available modules' do
+        before(:each) do
+          allow(Dir).to receive(:glob).with('../../modules/*')
+            .and_return(%w(foo bar))
+          allow(File).to receive(:exists?)
+            .with('./spec/fixtures/modules/profile').and_return(true)
+        end
+
+        context 'if discovered file is not a directory' do
+          it 'should not try to perform module operations on that file' do
+            allow(File).to receive(:directory?).with('./spec/fixtures/modules')
+              .and_return(true)
+            allow(File).to receive(:directory?).with('foo').and_return(false)
+            allow(File).to receive(:directory?).with('bar').and_return(false)
+            allow(File).to receive(:dirname).with(__FILE__).and_return('/tmp')
+            expect(File).to_not receive(:symlink)
+              .with('/tmp/foo', './spec/fixtures/modules/foo')
+            expect(File).to_not receive(:symlink)
+              .with('/tmp/bar', './spec/fixtures/modules/bar')
+            @dummy_class.profile_fixtures
+          end
+        end
+
+        context 'if discovered file is a directory' do
+          context 'if modules directories already are symlinks' do
+            it 'should not try to symlink the module path' do
+              allow(File).to receive(:directory?)
+                .with('./spec/fixtures/modules').and_return(true)
+              allow(File).to receive(:directory?).with('foo').and_return(true)
+              allow(File).to receive(:directory?).with('bar').and_return(true)
+              allow(File).to receive(:dirname).and_return('/tmp')
+              allow(File).to receive(:symlink?)
+                .with('./spec/fixtures/modules/foo').and_return(true)
+              allow(File).to receive(:symlink?)
+                .with('./spec/fixtures/modules/bar').and_return(true)
+              @dummy_class.profile_fixtures
+            end
+          end
+
+          context 'if modules directories do not already have symlinks' do
+            it 'should symlink the module path' do
+              allow(@dummy_class).to receive(:expanded_file_name)
+                .and_return('/tmp/control_spec_helper.rb')
+              allow(File).to receive(:directory?)
+                .with('./spec/fixtures/modules').and_return(true)
+              allow(File).to receive(:directory?)
+                .with('foo').and_return(true)
+              allow(File).to receive(:directory?)
+                .with('bar').and_return(true)
+              allow(File).to receive(:dirname)
+                .with(@dummy_class.expanded_file_name).and_return('/tmp')
+              allow(File).to receive(:symlink?)
+                .with('./spec/fixtures/modules/foo').and_return(false)
+              allow(File).to receive(:symlink?)
+                .with('./spec/fixtures/modules/bar').and_return(false)
+              expect(File).to receive(:symlink)
+                .with('/tmp/foo', './spec/fixtures/modules/foo')
+              expect(File).to receive(:symlink)
+                .with('/tmp/bar', './spec/fixtures/modules/bar')
+              @dummy_class.profile_fixtures
+            end
+          end
+
+          describe 'when debug environmental variable is set' do
+            before(:each) do
+              allow(@dummy_class).to receive(:profile_path).and_return('/')
+              allow(Dir).to receive(:glob).with('../../modules/*')
+                .and_return([])
+              allow(Dir).to receive(:pwd).and_return('/foo')
+              allow(File).to receive(:exists?)
+                .with('./spec/fixtures/modules/profile').and_return(true)
+              allow(FileUtils).to receive(:mkpath)
+                .with('./spec/fixtures/modules/')
+              cached_env_debug = ENV['debug']
+              ENV['debug'] = 'true'
+            end
+
+            after(:each) do
+              ENV['debug'] = cached_env_debug
+            end
+
+            it 'should print its current profile_path directory' do
+              expect { @dummy_class.profile_fixtures }
+                .to output(%r{DEBUG: cd to /}).to_stderr
+            end
+
+            it 'should print its actual working directory' do
+              expect { @dummy_class.profile_fixtures }
+                .to output(%r{DEBUG: cd to /foo}).to_stderr
+            end
+          end
+
+          context 'when debug environmental variable is not set' do
+            before(:each) do
+              allow(@dummy_class).to receive(:profile_path).and_return('/')
+              allow(Dir).to receive(:glob).with('../../modules/*')
+                .and_return([])
+              allow(Dir).to receive(:pwd).and_return('/foo')
+              allow(File).to receive(:exists?)
+                .with('./spec/fixtures/modules/profile').and_return(true)
+              allow(FileUtils).to receive(:mkpath)
+                .with('./spec/fixtures/modules/')
+            end
+
+            it 'should not print its current profile_path directory' do
+              cached_env_debug = ENV['debug']
+              ENV.delete('debug')
+              expect { @dummy_class.profile_fixtures }
+                .to_not output(%r{DEBUG: cd to /}).to_stderr
+              ENV['debug'] = cached_env_debug
+            end
+
+            it 'should not print its actual working directory' do
+              cached_env_debug = ENV['debug']
+              ENV.delete('debug')
+              expect { @dummy_class.profile_fixtures }
+                .to_not output(%r{DEBUG: cd to /foo}).to_stderr
+              ENV['debug'] = cached_env_debug
+            end
+          end
+
+          it 'should create a modules directory inside fixtures' do
+            expect(FileUtils).to receive(:mkpath)
+              .with('./spec/fixtures/modules/')
+            @dummy_class.profile_fixtures
+          end
+        end
+      end
     end
   end
+
   describe 'when spec_clean is called' do
-    describe 'when debug environmental variable is set' do
-      it 'should print its current project directory'
-      it 'should print its actual working directory'
+    before(:each) do
+      allow(@dummy_class).to receive(:project_root).and_return('/')
+      allow(@dummy_class).to receive(:profile_path).and_return('/tmp')
+      allow(Dir).to receive(:pwd).and_return('/foo')
     end
-    it 'should abort if fixtures is empty'
-    it 'should abort is fixtures is null'
-    it 'should abort if modules is empty'
-    it 'should abort if modules is null'
-    it 'calls the appropriate command to remove fixtures'
-    it 'calls the appropriate command to remove modules'
+
+    describe 'when debug environmental variable is set' do
+      it 'should print its current project directory' do
+        allow(@dummy_class).to receive(:`)
+          .with('rm -rf /tmp/spec/fixtures/modules/*')
+        allow(@dummy_class).to receive(:`).with('rm -rf /modules/*')
+        cached_env_debug = ENV['debug']
+        ENV['debug'] = 'true'
+        expect { @dummy_class.spec_clean }.to output(%r{DEBUG: cd to /})
+          .to_stderr
+        ENV['debug'] = cached_env_debug
+      end
+
+      it 'should print its actual working directory' do
+        allow(@dummy_class).to receive(:`)
+          .with('rm -rf /tmp/spec/fixtures/modules/*')
+        allow(@dummy_class).to receive(:`).with('rm -rf /modules/*')
+        cached_env_debug = ENV['debug']
+        ENV['debug'] = 'true'
+        expect { @dummy_class.spec_clean }.to output(%r{DEBUG: cd to /foo})
+          .to_stderr
+        ENV['debug'] = cached_env_debug
+      end
+    end
+
+    describe 'when debug environmental variable is not set' do
+      it 'should not print its current project directory' do
+        allow(@dummy_class).to receive(:`)
+          .with('rm -rf /tmp/spec/fixtures/modules/*')
+        allow(@dummy_class).to receive(:`).with('rm -rf /modules/*')
+        cached_env_debug = ENV['debug']
+        ENV.delete('debug')
+        expect { @dummy_class.spec_clean }.to_not output(%r{DEBUG: cd to /})
+          .to_stderr
+        ENV['debug'] = cached_env_debug
+      end
+
+      it 'should not print its actual working directory' do
+        allow(@dummy_class).to receive(:`)
+          .with('rm -rf /tmp/spec/fixtures/modules/*')
+        allow(@dummy_class).to receive(:`).with('rm -rf /modules/*')
+        cached_env_debug = ENV['debug']
+        ENV.delete('debug')
+        expect { @dummy_class.spec_clean }.to_not output(%r{DEBUG: cd to /})
+          .to_stderr
+        ENV['debug'] = cached_env_debug
+      end
+    end
+
+    # rubocop:disable Lint/UselessAssignment
+    it 'should abort if fixtures is empty' do
+      allow(@dummy_class).to receive(:`)
+        .with('rm -rf /tmp/spec/fixtures/modules/*')
+      allow(@dummy_class).to receive(:`).with('rm -rf /modules/*')
+      fixtures = ''
+      expect { @dummy_class.spec_clean }.to abort
+    end
+    # rubocop:enable Lint/UselessAssignment
+
+    # rubocop:disable Lint/UselessAssignment
+    it 'should abort if fixtures is null' do
+      allow(@dummy_class).to receive(:`)
+        .with('rm -rf /tmp/spec/fixtures/modules/*')
+      allow(@dummy_class).to receive(:`).with('rm -rf /modules/*')
+      fixtures = nil
+      expect { @dummy_class.spec_clean }.to abort
+    end
+    # rubocop:enable Lint/UselessAssignment
+
+    # rubocop:disable Lint/UselessAssignment
+    it 'should abort if modules is empty' do
+      allow(@dummy_class).to receive(:`)
+        .with('rm -rf /tmp/spec/fixtures/modules/*')
+      allow(@dummy_class).to receive(:`).with('rm -rf /modules/*')
+      modules = ''
+      expect { @dummy_class.spec_clean }.to abort
+    end
+    # rubocop:enable Lint/UselessAssignment
+
+    # rubocop:disable Lint/UselessAssignment
+    it 'should abort if modules is null' do
+      allow(@dummy_class).to receive(:`)
+        .with('rm -rf /tmp/spec/fixtures/modules/*')
+      allow(@dummy_class).to receive(:`).with('rm -rf /modules/*')
+      modules = nil
+      expect { @dummy_class.spec_clean }.to abort
+    end
+    # rubocop:enable Lint/UselessAssignment
+
+    it 'calls the appropriate command to remove fixtures' do
+      allow(@dummy_class).to receive(:`).with('rm -rf /modules/*')
+      expect(@dummy_class).to receive(:`)
+        .with('rm -rf /tmp/spec/fixtures/modules/*')
+      @dummy_class.clean_spec
+    end
+
+    it 'calls the appropriate command to remove modules' do
+      expect(@dummy_class).to receive(:`).with('rm -rf /modules/*')
+      allow(@dummy_class).to receive(:`)
+        .with('rm -rf /tmp/spec/fixtures/modules/*')
+      @dummy_class.clean_spec
+    end
   end
 end
