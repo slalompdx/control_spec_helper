e90bb39289c36ee9ceef06b3d97566f8f63c259b
Eric Shamow <alice@example.com>
2016-04-18 12:32:35 -0700

    Switch require to include for rake tasks

--- lib/control_spec_helper.rb
+++ lib/control_spec_helper.rb
@@ -1,3 +1,2 @@
 require 'control_spec_helper/control_spec_helper'
-puts "HALLO"
-Dir["./lib/tasks/**/*.rb"].sort.each { |f| require f ; puts f}
+Dir["./lib/tasks/**/*.rb"].sort.each { |f| include f }
