00da7557c1f729e082d2712dab183fda6eb7c46d
Eric Shamow <alice@example.com>
2016-04-18 10:52:06 -0700

    Move requires to spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,8 @@
 require 'rspec'
+require 'rake'
 require 'control_spec_helper'
+require 'rspec/core/rake_task'
+require 'puppet-lint/tasks/puppet-lint'
+require 'puppet-syntax/tasks/puppet-syntax'
 
 Dir["./spec/support/**/*.rb"].sort.each { |f| require f}
