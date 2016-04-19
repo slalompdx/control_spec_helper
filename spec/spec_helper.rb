a8ff9617e34636e4f62e8598cdbc27689be4017d
Eric Shamow <alice@example.com>
2016-04-19 09:15:46 -0700

    Modify to remove spec acceptance helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,5 +4,3 @@ require 'control_spec_helper'
 require 'rspec/core/rake_task'
 require 'puppet-lint/tasks/puppet-lint'
 require 'puppet-syntax/tasks/puppet-syntax'
-
-Dir["./spec/support/**/*.rb"].sort.each { |f| require f}
