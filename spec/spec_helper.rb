29fa0ccc8375e4adae4fd0caefa97adcdfed7002
Eric Shamow <alice@example.com>
2016-04-26 10:52:02 -0700

    First working acceptance test

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,3 +7,7 @@ require 'puppet-syntax/tasks/puppet-syntax'
 
 Dir.glob("#{File.dirname(__FILE__)}/../lib/tasks/**/*.rb")
    .each { |f| require f }
+Dir.glob("#{File.dirname(__FILE__)}/support/shared_contexts/**/*.rb")
+   .each { |f| require f }
+
+Rails_root = Pathname.new('..').expand_path(File.dirname(__FILE__))
