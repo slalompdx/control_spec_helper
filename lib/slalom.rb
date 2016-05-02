e9e12ec788226384d2562ec8bf26ea0fcaa3269e
Eric Shamow <alice@example.com>
2016-05-01 19:21:43 -0700

    Add ssh_exec! helper method

--- lib/slalom.rb
+++ lib/slalom.rb
@@ -1,10 +1,54 @@
+require 'net/ssh'
+require 'etc'
+
 def vagrant_ssh_config
   config = {}
-  `vagrant ssh-config --machine-readable`.split(',')[7]
+  `unset RUBYLIB ; vagrant ssh-config --machine-readable`.split(',')[7]
     .split('\n')[0..9].collect(&:lstrip)
     .each do |element|
       key, value = element.split(' ')
       config[key] = value
     end
   config
+end
+
+# Helper method to separate ssh output into streams and error code
+# Sample use:
+# Net::SSH.start(server, Etc.getlogin) do |ssh|
+#  puts ssh_exec!(ssh, "true").inspect
+#  # => ["", "", 0, nil]
+
+#  puts ssh_exec!(ssh, "false").inspect
+#  # => ["", "", 1, nil]
+# end
+
+def ssh_exec!(ssh, command)
+  stdout_data = ""
+  stderr_data = ""
+  exit_code = nil
+  exit_signal = nil
+  ssh.open_channel do |channel|
+    channel.exec(command) do |ch, success|
+      unless success
+        abort "FAILED: couldn't execute command (ssh.channel.exec)"
+      end
+      channel.on_data do |ch,data|
+        stdout_data+=data
+      end
+
+      channel.on_extended_data do |ch,type,data|
+        stderr_data+=data
+      end
+
+      channel.on_request("exit-status") do |ch,data|
+        exit_code = data.read_long
+      end
+
+      channel.on_request("exit-signal") do |ch, data|
+        exit_signal = data.read_long
+      end
+    end
+  end
+  ssh.loop
+  [stdout_data, stderr_data, exit_code, exit_signal]
 end
