9e05dcdbd764df8aae322f582523c592bb423b87
Eric Shamow <alice@example.com>
2016-05-03 23:47:41 -0700

    Suppress output

--- spec/tasks/vplugins_spec.rb
+++ spec/tasks/vplugins_spec.rb
@@ -7,16 +7,32 @@ require 'net/ssh'
 
 describe :vplugins do
   include_context 'rake'
+
   before(:all) do
     @original_dir = Dir.pwd
     Dir.chdir('fixtures/puppet-control')
   end
+
   after(:all) do
     Dir.chdir(@original_dir)
   end
 
   context 'when run on CentOS 7' do
+    cached_env_debug = ''
+    original_stderr = $stderr
+    original_stdout = $stdout
+
     let(:conf) { ssh_config }
+
+    before(:each) do
+      $stderr = File.open(File::NULL, 'w')
+      $stdout = File.open(File::NULL, 'w')
+    end
+
+    after(:each) do
+      $stderr = original_stderr
+      $stdout = original_stdout
+    end
 
     it 'should install successfully' do
       expect(Net::SSH.start(
@@ -29,6 +45,7 @@ describe :vplugins do
                   'cd /vagrant && unset RUBYLIB ; bundle exec rake vplugins')
       end[2]).to eq(0)
     end
+
     it 'should install the vagrant-auto_network plugin' do
       expect(Net::SSH.start(
         conf['HostName'],
@@ -39,6 +56,7 @@ describe :vplugins do
         ssh_exec!(ssh, 'unset RUBYLIB ; vagrant plugin list')
       end[0].split("\n")).to include(/vagrant-auto_network/)
     end
+
     it 'should install the vagrant-hosts plugin' do
       expect(Net::SSH.start(
         conf['HostName'],
